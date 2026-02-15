58ddcb5522d49c53d4da1685848bd34c926aba78
Prem Supthaksina <PremSupthaksina@gmail.com>
2026-02-15 21:32:26 +0700

    TEST

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           src="/assets/PORT_HOME.png"
           alt="Prem"
           className="w-100 shadow"
-          style={{ height: "80vh", objectFit: "cover" }}
+          style={{ height: "40vh", objectFit: "cover" }}
         />
       </div>
 
